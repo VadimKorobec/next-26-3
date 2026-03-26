fd36463ce9afd965dbfca504daf3f66ee2b87ce7
Vadim <_s_e_t__@ukr.net>
2026-03-26 14:15:52 +0200

    create parallel routes

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { notFound } from "next/navigation";
+
 import { DUMMY_NEWS } from "../../../../dummy-news";
 
 interface NewsDetailePageProps {
@@ -9,24 +11,24 @@ const NewsDetailePage = async ({ params }: NewsDetailePageProps) => {
   const { slug } = await params;
   const newsItem = DUMMY_NEWS.find((item) => item.slug === slug);
 
+  if (!newsItem) {
+    notFound();
+  }
+
   return (
     <article className="news-article">
-      {newsItem ? (
-        <header>
-          <Image
-            src={`/images/news/${newsItem.image}`}
-            alt={newsItem.title}
-            width={100}
-            height={100}
-            loading="eager"
-          />
-          <h1>{newsItem.title}</h1>
-          <time dateTime={newsItem.date}>{newsItem.date}</time>
-          <p>{newsItem.content}</p>
-        </header>
-      ) : (
-        <p>Sorry we do not have item</p>
-      )}
+      <header>
+        <Image
+          src={`/images/news/${newsItem.image}`}
+          alt={newsItem.title}
+          width={100}
+          height={100}
+          loading="eager"
+        />
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+        <p>{newsItem.content}</p>
+      </header>
     </article>
   );
 };
