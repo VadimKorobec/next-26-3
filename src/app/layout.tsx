35e06e2b97ddd8ac12c93dc5c9d30de76cfc821d
Vadim <_s_e_t__@ukr.net>
2026-03-26 11:51:15 +0200

    add MainHeader component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import MainHeader from "@/components/MainHeader/MainHeader";
+
 import "./globals.css";
 
 export const metadata = {
@@ -12,7 +14,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div id="page">
+          <MainHeader />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,21 +1,23 @@
 import Link from "next/link";
 
+import { DUMMY_NEWS } from "../../../dummy-news";
+import Image from "next/image";
+
 const NewsPage = () => {
   return (
-    <div>
+    <>
       <h1>News Page</h1>
-      <ul>
-        <li>
-          <Link href="/first">First News Item</Link>
-        </li>
-        <li>
-          <Link href="/second">Second News Item</Link>
-        </li>
-        <li>
-          <Link href="/third">Third News Item</Link>
-        </li>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((item) => (
+          <li key={item.id}>
+            <Link href={`/news/${item.slug}`}>
+              <Image src={`/images/news/${item.image}`} alt={item.title} width={200} height={200} />
+              <span>{item.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
-    </div>
+    </>
   );
 };
 
