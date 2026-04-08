e1ebfbf1dfe3320c7078514bd76fb9a5ed686f2e
Vadim <_s_e_t__@ukr.net>
2026-04-08 15:46:51 +0300

    add some code

---
+++ src/app/news/[slug]/@modal/(.)image/page.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { DUMMY_NEWS } from "../../../../../../dummy-news";
+
+interface ImagePageProps {
+  params: Promise<{ slug: string }>;
+}
+
+const ImagePage = async ({ params }: ImagePageProps) => {
+  const { slug } = await params;
+  const newsItem = DUMMY_NEWS.find((item) => item.slug === slug);
+
+  if (!newsItem) {
+    notFound();
+  }
+
+  return (
+    <>
+      <div className="modal-backdrop" />
+      <dialog className="modal" open>
+        <div className="fullscreen-image">
+          <Image
+            src={`/images/news/${newsItem.image}`}
+            alt={newsItem.title}
+            fill
+          />
+        </div>
+      </dialog>
+    </>
+  );
+};
+
+export default ImagePage;
