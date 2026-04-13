9128cb7ca1db9e0c3cac27a2dabf8fe9a1543c7c
Vadim <_s_e_t__@ukr.net>
2026-04-13 15:05:57 +0300

    add some code

---
+++ src/app/(content)/layout.tsx
@@ -0,0 +1,23 @@
+import MainHeader from "@/components/MainHeader/MainHeader";
+
+import "./globals.css";
+
+export const metadata = {
+  title: "Next.js Page Routing & Rendering",
+  description: "Learn how to route to different pages.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <MainHeader/>
+        <div id="page">{children}</div>
+      </body>
+    </html>
+  );
+}
